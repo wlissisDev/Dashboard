bea80aac748a7ec51d883b7e293806e6effe17eb
wlissisdev <wlissisrds@outlook.com>
2022-12-06 10:24:58 -0300

    widget-concluido

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './home.scss';
-import {Sidebar} from '../../components/sidebar/Sidebar';
+import { Sidebar } from '../../components/sidebar/Sidebar';
 import { Navbar } from '../../components/navbar/Navbar';
+import { Widget } from '../../components/widget/Widget';
 
 export const Home = () => {
   return (
     <div className='home'>
       <Sidebar></Sidebar>
       <div className="homeContainer">
-      <Navbar/>
-        Container
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   )
